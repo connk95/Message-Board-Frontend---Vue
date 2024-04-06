7e8031db646da3062ecb25c211ede95e4c9f8635
Connor Ketcheson <connorketcheson01@gmail.com>
2024-04-06 13:42:45 +0900

    adjust styling

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,45 +1,54 @@
 <template>
-  <v-row justify="center" style="margin-top: 12vh">
-    <v-col cols="8">
-      <v-row style="margin-left: 1.5vw">
-        <v-typography style="margin-bottom: 2vh" class="text-h4"> All posts </v-typography>
-      </v-row>
-      <v-row style="margin-left: 1.5vw">
-        <v-btn
-          v-if="user"
-          @click="goToNewPostPage"
-          color="#23532c"
-          style="margin-bottom: 2vh; text-transform: unset"
-          class="text-body-1"
+  <v-container>
+    <v-row
+      justify="center"
+      direction="column"
+      class="align-center"
+      style="margin-top: 12vh; width: 100vh"
+    >
+      <v-col cols="8">
+        <v-row style="margin-left: 1.5vw" direction="column" class="align-start">
+          <v-typography style="margin-bottom: 2vh" class="text-h4"> All posts </v-typography>
+          <!-- </v-row> -->
+          <!-- <v-row style="margin-left: 1.5vw"> -->
+        </v-row>
+        <v-row style="margin-left: 1.5vw" direction="column" class="align-start">
+          <v-btn
+            v-if="user"
+            @click="goToNewPostPage"
+            color="#23532c"
+            style="width: 6rem; margin-bottom: 2vh; text-transform: unset"
+            class="text-body-1"
+          >
+            New Post
+          </v-btn>
+        </v-row>
+        <v-progress-circular
+          v-if="loading"
+          indeterminate
+          color="black"
+          style="margin-left: 2vw; margin-top: 2vh"
+        ></v-progress-circular>
+        <v-card
+          v-else
+          v-for="post in reversedPosts"
+          :key="post._id"
+          style="margin: 20px; width: 60vw"
+          elevation="2"
+          class="pb-2 pt-2"
         >
-          New Post
-        </v-btn>
-      </v-row>
-      <v-progress-circular
-        v-if="loading"
-        indeterminate
-        color="black"
-        style="margin-left: 2vw; margin-top: 2vh"
-      ></v-progress-circular>
-      <v-card
-        v-else
-        v-for="post in reversedPosts"
-        :key="post._id"
-        style="margin: 20px; width: 60vw"
-        elevation="2"
-        class="pb-2 pt-2"
-      >
-        <router-link :to="`/posts/${post._id}`" style="text-decoration: none; color: inherit">
-          <v-card-title class="text-h6 pb-0">{{ post.title }}</v-card-title>
-          <v-card-text class="text-body-1 pb-0">{{ post.text }}</v-card-text>
-          <v-card-text class="text-body-2 pb-0 pt-1">
-            posted at {{ post.createdAt.slice(11, 16) }} on {{ post.createdAt.slice(0, 10) }}
-          </v-card-text>
-          <v-card-text class="text-body-2 pt-0"> by {{ post.user.username }} </v-card-text>
-        </router-link>
-      </v-card>
-    </v-col>
-  </v-row>
+          <router-link :to="`/posts/${post._id}`" style="text-decoration: none; color: inherit">
+            <v-card-title class="text-h6 pb-0">{{ post.title }}</v-card-title>
+            <v-card-text class="text-body-1 pb-0">{{ post.text }}</v-card-text>
+            <v-card-text class="text-body-2 pb-0 pt-1">
+              posted at {{ post.createdAt.slice(11, 16) }} on {{ post.createdAt.slice(0, 10) }}
+            </v-card-text>
+            <v-card-text class="text-body-2 pt-0"> by {{ post.user.username }} </v-card-text>
+          </router-link>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,14 +1,14 @@
 <template>
-  <v-row justify="center" direction="column" style="margin-top: 12vh">
-    <v-col cols="8">
-      <v-progress-circular
-        v-if="loading"
-        indeterminate
-        color="black"
-        style="margin-left: 2vw; margin-top: 2vh"
-      ></v-progress-circular>
-      <v-row v-else-if="singlePost">
-        <v-card style="margin: 20px; width: 60vw" elevation="2" class="pb-2 pt-2">
+  <v-container>
+    <v-row class="align-center mt-14">
+      <v-col cols="8">
+        <v-progress-circular
+          v-if="loading"
+          indeterminate
+          color="black"
+          style="margin-left: 2vw; margin-top: 2vh; width: 60vw"
+        ></v-progress-circular>
+        <v-card v-else-if="singlePost" style="width: 60vw" elevation="2" class="pa-2 w-60">
           <v-card-title class="text-h6 pb-0">{{ singlePost.title }}</v-card-title>
           <v-card-text class="text-body-1 pb-0">{{ singlePost.text }}</v-card-text>
           <v-card-text class="text-body-2 pb-0 pt-1">
@@ -17,12 +17,21 @@
           </v-card-text>
           <v-card-text class="text-body-2 pt-0"> by {{ singlePost.user.username }} </v-card-text>
         </v-card>
-        <v-typography v-if="commentNumber == 0" style="margin-bottom: 10px; margin-left: 20px"
+        <v-typograpgy v-else type="error">Post not found</v-typograpgy>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-typography v-if="comments.length == 0" style="margin-bottom: 10px; margin-left: 20px"
           >Be the first to leave a comment!</v-typography
         >
         <v-typography v-else style="margin-bottom: 10px; margin-left: 20px"
           >Comments: {{ reversedComments.length }}</v-typography
         >
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
         <v-card
           v-for="comment in reversedComments"
           :key="comment._id"
@@ -33,20 +42,40 @@
           <v-card-title class="text-h6 pb-0">{{ comment.title }}</v-card-title>
           <v-card-text class="text-body-1 pb-0">{{ comment.text }}</v-card-text>
           <v-card-text class="text-body-2 pb-0 pt-1">
-            posted at {{ comment.createdAt.slice(11, 16) }} on {{ comment.createdAt.slice(0, 10) }}
+            posted at {{ comment.createdAt.slice(11, 16) }} on
+            {{ comment.createdAt.slice(0, 10) }}
           </v-card-text>
           <v-card-text class="text-body-2 pt-0"> by {{ comment.user.username }} </v-card-text>
         </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
         <v-text-field
           v-if="user"
           v-model="comment"
           label="Comment"
           style="width: 60vw; margin-left: 20px; margin-right: 20px; margin-top: 10px"
         ></v-text-field>
-      </v-row>
-      <v-alert v-else type="error">Post not found</v-alert>
-    </v-col>
-  </v-row>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn
+          color="#23532c"
+          style="width: 6rem; margin-bottom: 2vh; text-transform: unset"
+          class="text-body-1"
+          >Submit</v-btn
+        >
+        <v-btn
+          color="#23532c"
+          style="width: 6rem; margin-bottom: 2vh; text-transform: unset"
+          class="text-body-1"
+          >Back</v-btn
+        >
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -80,8 +109,8 @@ export default defineComponent({
         ? singlePost.value.comments.slice().reverse()
         : []
     })
-    const commentNumber = computed(() => {
-      return singlePost.value.comments.length
+    const comments = computed(() => {
+      return singlePost.value.comments
     })
 
     return {
@@ -89,7 +118,7 @@ export default defineComponent({
       loading,
       user,
       reversedComments,
-      commentNumber
+      comments
     }
   }
 })
